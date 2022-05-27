5804f43674b66bb5d30d3572a6b3dd76e03f47e7
Daniel-Marttins <daniel.marttinsx@gmail.com>
2022-05-27 16:22:53 -0300

    Banco de dados h2 e a criação automatica da base de dados

--- src/main/java/com/retribution/cursomc/domain/CategoriaDomain.java
+++ src/main/java/com/retribution/cursomc/domain/CategoriaDomain.java
@@ -1,11 +1,18 @@
 package com.retribution.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class CategoriaDomain implements Serializable {
     private  static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 

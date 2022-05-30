080b449a007b0be23faee01643abdafc0edb9efe
Daniel-Marttins <daniel.marttinsx@gmail.com>
2022-05-30 12:19:15 -0300

    Criando operação de instanciação

--- src/main/java/com/retribution/cursomc/CursomcApplication.java
+++ src/main/java/com/retribution/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.retribution.cursomc;
 
+import com.retribution.cursomc.domain.CategoriaDomain;
+import com.retribution.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		CategoriaDomain cat1 = new CategoriaDomain(null, "Informática");
+		CategoriaDomain cat2 = new CategoriaDomain(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
